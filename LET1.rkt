d8d006ead732370332805eb5e93cbdb3a3bba98e
Jin Ende <jinende@teach.cs.toronto.edu>
2017-09-28 17:27:49 -0400

    Change to Haskell as implmentation now. Because I don't want/know to test. I will use type to check.

--- LET1.rkt
+++ LET1.rkt
@@ -1,5 +1,5 @@
-#lang eopl
 
+#lang eopl
 
 (define (extend-env sym bind env)
   (lambda (lookfor)
@@ -9,11 +9,17 @@
 
 (define empty-env (lambda (x) `UNBOUND_VARIABLE))
 
+
 (define-datatype Expval expval?
   (num-val
    (num number?))
   (bool-val
-   (bool boolean?)))
+   (bool boolean?))
+  (pair-val
+   (fst expval?)
+   (tail expval?))
+  (emptylist-val)
+  )
 
 (define (expval->num expv)
   (cases Expval expv
@@ -25,11 +31,31 @@
     (bool-val (n) n)
     (else `TYPE_INCONSISTENT)))
 
+(define (expval->pair expv)
+  (cases Expval expv
+    (pair-val (a b) (cons a b))
+    (else `TYPE_INCONSISTENT)))
 
 
 (define-datatype program program?
   (a-program
    (expl expression?)))
+
+(define (list-of-sth sth?)
+  (define (LP? l)
+    (or (eq? l '())
+    (and (pair? l)
+         (and (sth? (car l))
+              (LP? (cdr l))))))
+  LP?)
+
+(define (pair-of-a-b a? b?)
+  (define (predic p)
+    (and (pair? p)
+         (a? (car p))
+         (b? (cdr p))))
+  predic
+  )
 
 (define-datatype expression expression?
   (const-exp
@@ -48,7 +74,43 @@
   (let-exp
    (sym symbol?)
    (bind expression?)
-   (expr expression?)))
+   (expr expression?))
+  (neg-exp
+   (num expression?))
+  (add-exp
+   (num0 expression?)
+   (num1 expression?))
+  (mult-exp
+   (num0 expression?)
+   (num1 expression?))
+  (divide-exp
+   (num0 expression?)
+   (num1 expression?))
+  (equal-exp
+   (num0 expression?)
+   (num1 expression?))
+  (greater-exp
+   (num0 expression?)
+   (num1 expression?))
+  (less-exp
+   (num0 expression?)
+   (num1 expression?))
+  (cons-exp
+   (e1 expression?)
+   (e2 expression?))
+  (car-exp
+   (e expression?))
+  (cdr-exp
+   (e expression?))
+  (null?-exp
+   (e expression?))
+  (emptylist-exp)
+  (list-exp
+   (le (list-of-sth expression?)))
+  (cond-exp
+   (lpe (list-of-sth (pair-of-a-b expression? expression?))))
+  
+  )
 ; env :: [(symbol, expval)]
 ; value-of :: expression -> env -> expval
 (define (value-of expres env)
@@ -75,5 +137,76 @@
              (let ([binding (value-of bind)])
                (value-of body (extend-env sym binding env))))
 
+    (neg-exp (n)
+             (num-val (- (expval->num (value-of n env)))))
+
+    (add-exp (a b)
+              (let ([num0 (value-of a env)]
+                    [num1 (value-of b env)])
+                (num-val (+ (expval->num num0) (expval->num num1)))))
+    (mult-exp (a b)
+              (let ([num0 (value-of a env)]
+                    [num1 (value-of b env)])
+                (num-val (* (expval->num num0) (expval->num num1)))))
+
+    (divide-exp (a b)
+              (let ([num0 (value-of a env)]
+                    [num1 (value-of b env)])
+                (num-val (quotient (expval->num num0) (expval->num num1)))))
+
+    (equal-exp (a b)
+               (let ([num0 (value-of a env)]
+                    [num1 (value-of b env)])
+                (bool-val (equal? (expval->num num0) (expval->num num1)))))
+    (greater-exp (a b)
+               (let ([num0 (value-of a env)]
+                    [num1 (value-of b env)])
+                (bool-val (> (expval->num num0) (expval->num num1)))))
+               
+    (less-exp (a b)
+               (let ([num0 (value-of a env)]
+                    [num1 (value-of b env)])
+                (bool-val (< (expval->num num0) (expval->num num1)))))
+
+    (cons-exp (a b)
+              (let ([fst (value-of a env)]
+                    [snd (value-of b env)])
+                (pair-val fst snd)))
+
+    (car-exp (a)
+             (car (expval->pair (value-of a env))))
+    (cdr-exp (a)
+             (cdr (expval->pair (value-of a env))))
+    (null?-exp (a)
+               (cases Expval (value-of a env)
+                 (emptylist-val () (bool-val #t))
+                 (else (bool-val #f))))
+    (emptylist-exp ()
+                   emptylist-val)
+    (list-exp (l)
+              (listexp->listexpval l))
+    (cond-exp (l)
+              (eval-condexp l env))
+              
+    
     (else `UNEXPECTED_PARAMETER)))
+
+(define (listexp->listexpval le env)
+  (cond
+    [(pair? le) (pair-val (value-of (car le env))
+                         (listexp->listexpval (cdr le) env))]
+    [else (value-of le env)]))
+
+(define (eval-condexp les env)
+  (cond
+    [(pair? les) (let ([cond-result (car les)]
+                       [else-route (cdr les)])
+                   (let ([condition (car cond-result)]
+                         [result (cdr cond-result)])
+                     (if (expval->bool (value-of condition env))
+                         (value-of result env)
+                         (eval-condexp else-route env))))]
+    [else `ERROR-NO_RETURN_VAL]))
+            
+                                       
             
